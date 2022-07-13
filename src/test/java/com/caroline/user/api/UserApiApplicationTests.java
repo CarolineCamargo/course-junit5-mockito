5da2934ff6326bfd6542b3c7a1153b2289835ca8
Caroline de Camargo Flach <caroline.camargo@squadra.com.br>
2022-07-13 12:41:45 -0300

    Created User entity

---
+++ src/test/java/com/caroline/user/api/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caroline.user.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
